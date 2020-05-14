114001694bbed7652186caaf3e93a1ed0c8d4dbe
Blazko381 <mkdante381@gmail.com>
2020-05-14 12:15:52 +0200

    Update mdss_mdp_kcal_ctrl.c

--- drivers/video/msm/mdss/mdss_mdp_kcal_ctrl.c
+++ drivers/video/msm/mdss/mdss_mdp_kcal_ctrl.c
@@ -580,12 +580,12 @@ static int kcal_ctrl_probe(struct platform_device *pdev)
 	lut_data->red = DEF_PCC;
 	lut_data->green = DEF_PCC;
 	lut_data->blue = DEF_PCC;
-	lut_data->minimum = 0x23;
+	lut_data->minimum = 0x43;
 	lut_data->invert = 0x0;
 	lut_data->hue = 0x0;
-	lut_data->sat = DEF_PA;
-	lut_data->val = DEF_PA;
-	lut_data->cont = DEF_PA;
+	lut_data->sat = DEF_PA-0x0d;
+	lut_data->val = DEF_PA+0x02;
+	lut_data->cont = DEF_PA-0x02;
 
 	lut_data->queue_changes = false;
 
